92d3ff50e9894eb62406719fc4d8dfce41dff018
MalkGanMu <malkganmu@163.com>
2025-03-13 17:10:36 +0800

    初始化

---
+++ tgx-metaverse-client/assets/res/tiledSet/flag_b.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="flag_b" tilewidth="16" tileheight="16" tilecount="10" columns="10">
+ <image source="../tiledRes/flag_b.png" width="160" height="16"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="9" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
